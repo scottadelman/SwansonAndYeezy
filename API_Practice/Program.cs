552dd20ef3c8e1b3175177d079b5ab55f0e5d58c
Scott <ScottAdelman97@gmail.com>
2022-08-02 13:48:01 -0600

    for loop added for swanson and yeezy

--- API_Practice/Program.cs
+++ API_Practice/Program.cs
@@ -3,32 +3,18 @@ using System;
 using System.Linq;
 using System.Text;
 
+
 var client = new HttpClient();
-
-
+var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 var kanyeURL = "https://api.kanye.rest/";
 
-var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
 
-var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
-
-var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-
-var swansonResponse = client.GetStringAsync(swansonURL).Result;
-
-var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
-
-
-Console.WriteLine(kanyeQuote);
-Console.WriteLine();
-Console.WriteLine(swansonQuote);
-Console.WriteLine();
-Console.WriteLine(kanyeQuote);
-Console.WriteLine();
-Console.WriteLine(swansonQuote);
-Console.WriteLine();
-Console.WriteLine(kanyeQuote);
-Console.WriteLine();
-Console.WriteLine(swansonQuote);
-Console.WriteLine();
-Console.WriteLine(kanyeQuote);
+for (int i = 0; i < 6; i++)
+{
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    var swansonResponse = client.GetStringAsync(swansonURL).Result;
+    var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+    Console.WriteLine($"Kanye: {kanyeQuote} \n");
+    Console.WriteLine($"Ron: {swansonQuote} \n");
+}
